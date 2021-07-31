14c7d3a758f66c7c813a95dcb854fcc686641e85
Akshay <akshay@nighthack.in>
2021-07-31 14:20:35 +0530

    0x000000000-640f36145bb63d6ddc2e54c2ad3e1461

---
+++ check.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+const { gitlogPromise } = require("gitlog");
+
+const options = {
+  repo: __dirname + "/",
+  fields: ["hash", "abbrevHash", "subject", "authorName", "authorDateRel"],
+};
+
+async function checkBlockchain() {
+  try {
+    let remoteCommits = await axios.get(
+      "https://api.github.com/repos/akshay8033/AuthPoliceV2/commits"
+    );
+
+    const localCommits = await gitlogPromise(options);
+
+    for (let i = 0; i < remoteCommits.data.length; i++) {
+      if (remoteCommits.data[i].commit.message != localCommits[i - 1].subject) {
+        throw Error("Hash mismatch");
+      }
+    }
+
+    const newCommit = localCommits[0].subject.split("-");
+    if (newCommit[0] != remoteCommits.data[0].sha) {
+      throw Error("Hash mismatch");
+    }
+
+    console.log("All good. Ready to add the transaction!");
+  } catch (err) {
+    console.log("Blockchain is tampered");
+    console.log(err);
+  }
+}
+
+checkBlockchain();
